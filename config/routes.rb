abb2ff9bada0912ab73f5f0377dbb4564da3f7e6
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-10 22:16:13 +0100

    add root route and callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
   resources :users
+
+  match 'auth/shopify/callback', to:  'users#callback', via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
