8688cb19e486c7fdddf3867ef8b7967d5d19310d
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-10 21:50:14 +0100

    add callback method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to @user.auth_url }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -59,6 +58,44 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def callback
+    p params['code']
+    p params['hmac']
+    store =  params['shop']
+    p params['state']
+    p params['timestamp']
+
+        body = {
+              "client_id": ENV['SHOPIFY_API_KEY'],
+              "client_secret": ENV['SHOPIFY_API_SECRET_KEY'],
+              "code": params['code']
+        }
+        url = URI("https://#{params['shop']}/admin/oauth/access_token")
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        p url
+        p http
+
+        request = Net::HTTP::Post.new(url)
+        request["content-type"] = 'application/json'
+        request["cache-control"] = 'no-cache'
+        request.body = body.to_json
+
+        response = JSON.parse(http.request(request).body)
+
+        user = User.find_by_store(store)
+        if user.update_attributes(:access_token => response['access_token'], :scopes => response['scope'])
+          # p 'updated successfully'
+          GetCustomersJob.perform_later(user.id)
+          redirect_to root_path
+        end
+        # p store
+        # p response['access_token']
+        # p response['scope']
+        # p response
   end
 
   private
