9b5c6ea1d6650ee66dc3ce1de9556c15f74cf164
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-10 22:01:27 +0100

    create redirect url for 0auth2 call

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :customers, dependent: :destroy
+
+
+    def auth_url
+        api_key = ENV['SHOPIFY_API_KEY']
+        scopes = 'read_customers'
+        redirect_uri = 'https://localhost:3000/auth/shopify/callback'
+        nonce = 'ABCDEF'
+        'https://'+ self.store + '/admin/oauth/authorize?client_id='+ ENV['SHOPIFY_API_KEY'] +'&scope='+ scopes +'&redirect_uri='+ redirect_uri +'&state='+ nonce +'&grant_options[]='
+    end
 end
