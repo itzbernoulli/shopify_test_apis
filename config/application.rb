34b3ae7a01ff4a20d6083dfba1d727ca1c3d94ac
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-11 17:43:23 +0100

    Remove autoloader code snippet due to heroku failure

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module ShopifyTest
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
