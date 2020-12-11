b5f71e2b1156e9369039a78c9cb0ec4213807ee7
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-11 12:53:42 +0100

    Verify data coming from shopify(WIP)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :customers, dependent: :destroy
 
+    validates :store, format: { with: /[a-zA-Z0-9][a-zA-Z0-9\-]*\.myshopify\.com[\/]?/, message: "shopify url in invalid." }
+    validates_uniqueness_of :store
 
     def auth_url
         api_key = ENV['SHOPIFY_API_KEY']
@@ -9,4 +11,13 @@ class User < ApplicationRecord
         nonce = 'ABCDEF'
         'https://'+ self.store + '/admin/oauth/authorize?client_id='+ ENV['SHOPIFY_API_KEY'] +'&scope='+ scopes +'&redirect_uri='+ redirect_uri +'&state='+ nonce +'&grant_options[]='
     end
+
+    def vefiry_response(json)
+
+    end
+    def check_nonce(nonce)
+        nonce == self.nonce
+    end
+
+
 end
