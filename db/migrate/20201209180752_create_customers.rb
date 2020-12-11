2a8b9ed0d6cd65be07e312b3dc7e1c54a730c07d
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-11 17:49:35 +0100

    remove id from customer migration due to conflicts

--- db/migrate/20201209180752_create_customers.rb
+++ db/migrate/20201209180752_create_customers.rb
@@ -2,7 +2,6 @@ class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers, id: :uuid do |t|
       t.references :user, null: false, foreign_key: true, type: :uuid
-      t.string :id
       t.string :first_name
       t.string :last_name
       t.string :email
