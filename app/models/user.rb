73e26f2bf926d940b9bbeb63c6e46205995f3877
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-11 17:15:40 +0100

    Auto generate nonce

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,22 @@ class User < ApplicationRecord
     validates :store, format: { with: /[a-zA-Z0-9][a-zA-Z0-9\-]*\.myshopify\.com[\/]?/, message: "shopify url in invalid." }
     validates_uniqueness_of :store
 
+    before_save :generate_nonce
+
     def auth_url
         api_key = ENV['SHOPIFY_API_KEY']
         scopes = 'read_customers'
         redirect_uri = 'https://localhost:3000/auth/shopify/callback'
-        nonce = 'ABCDEF'
-        'https://'+ self.store + '/admin/oauth/authorize?client_id='+ ENV['SHOPIFY_API_KEY'] +'&scope='+ scopes +'&redirect_uri='+ redirect_uri +'&state='+ nonce +'&grant_options[]='
+        'https://'+ self.store + '/admin/oauth/authorize?client_id='+ ENV['SHOPIFY_API_KEY'] +'&scope='+ scopes + '&redirect_uri='+ redirect_uri +'&state='+ self.nonce + '&grant_options[]='
     end
 
-    def vefiry_response(json)
-
+    def check_nonce=(nonce)
+        nonce.eql? self.nonce
     end
-    def check_nonce(nonce)
-        nonce == self.nonce
+
+    private 
+    def generate_nonce
+        self.nonce = SecureRandom.uuid[0..5]
     end
 
 
