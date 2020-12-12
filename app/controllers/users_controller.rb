f27ddea60c8be94f05b8b79469806cc62868b82a
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-12 14:27:21 +0100

    Update edit action to update customer list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    UpdateCustomerListJob.perform_later(@user.id)
+    redirect_to root_url, notice: "Update in progress"
   end
 
   # POST /users
@@ -31,20 +33,6 @@ class UsersController < ApplicationController
         format.html { redirect_to @user.auth_url }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
