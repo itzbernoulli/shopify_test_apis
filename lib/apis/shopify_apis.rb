d0c1d0e86b300ea1cb8cfb8dc3ea5d50c819c432
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-15 00:08:49 +0100

    refactor response and user update method

--- lib/apis/shopify_apis.rb
+++ lib/apis/shopify_apis.rb
@@ -1,50 +1,63 @@
 module ShopifyApi
   class Shopify
-    require 'apis/shopify_request'
-    HTTP_ERRORS = [
-      Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
-      Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
-    ].freeze
+    class << self
+      require 'apis/shopify_request'
 
-    OAUTH_URL = '/admin/oauth/access_token'.freeze
-    CUSTOMERS_URL = '/admin/api/2020-10/customers.json?fields=first_name,last_name,email'.freeze
-    
-    def self.get_access_token(user, code)
-      begin
-        body = { "client_id": ENV['SHOPIFY_API_KEY'], "client_secret": ENV['SHOPIFY_API_SECRET_KEY'], "code": code }
-        url = URI("https://#{user.store}" + OAUTH_URL)
-        ShopifyRequest.get_token(url, body)
-      rescue *HTTP_ERRORS
-        raise
-      end
-    end
+      ACTIONS = [CREATE = 'create'.freeze, UPDATE = 'update'.freeze].freeze
 
-    def self.get_store_customers(user)
-      begin
-        url = URI("https://#{user.store}" + CUSTOMERS_URL)
-        verification_response = ShopifyRequest.get_request(url, user)
-        if verification_response['customers'].count.positive?
-          Customer.insert_all(verification_response['customers'], user.id)
-          user.update_attribute(:last_customer_update_time, DateTime.now)
+      HTTP_ERRORS = [
+        Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError
+      ].freeze
+
+      OAUTH_URL = '/admin/oauth/access_token'.freeze
+      CUSTOMERS_URL = '/admin/api/2020-10/customers.json?fields=first_name,last_name,email'.freeze
+      
+      def get_access_token(user, code)
+        begin
+          body = { "client_id": ENV['SHOPIFY_API_KEY'], "client_secret": ENV['SHOPIFY_API_SECRET_KEY'], "code": code }
+          url = URI("https://#{user.store}" + OAUTH_URL)
+          ShopifyRequest.get_token(url, body)
+        rescue *HTTP_ERRORS
+          raise
         end
-      rescue *HTTP_ERRORS
-        raise
       end
-    end
 
-    def self.update_store_customer_list(user)
-      begin
-        url = URI("https://#{user.store}#{CUSTOMERS_URL}&created_at_min=
-        #{user.last_customer_update_time}&updated_at_min=#{user.last_customer_update_time}")
-        verification_response = ShopifyRequest.get_request(url, user)
-        if verification_response['customers'].count.positive?
-          Customer.upsert_all(verification_response['customers'], user.id)
-          user.update_attribute(:last_customer_update_time, DateTime.now)
+      def get_store_customers(user)
+        begin
+          url = URI("https://#{user.store}" + CUSTOMERS_URL)
+          create_or_update_customers(CREATE, ShopifyRequest.get_request(url, user), user)
+        rescue *HTTP_ERRORS
+          raise
         end
-      rescue *HTTP_ERRORS
-        raise
+      end
+
+      def update_store_customer_list(user)
+        begin
+          url = URI("https://#{user.store}#{CUSTOMERS_URL}&created_at_min=
+          #{user.last_customer_update_time}&updated_at_min=#{user.last_customer_update_time}")
+          create_or_update_customers(UPDATE, ShopifyRequest.get_request(url, user), user)
+        rescue *HTTP_ERRORS
+          raise
+        end
+      end
+
+      private 
+      
+      def update_user(user)
+        user.update_attribute(:last_customer_update_time, DateTime.now)
+      end
+
+      def create_or_update_customers(action, response, user)
+        return unless response['customers'].count.positive?
+
+        if action.eql? CREATE
+          Customer.insert_all(response['customers'], user.id)
+        else
+          Customer.upsert_all(response['customers'], user.id)
+        end
+        update_user(user)
       end
     end
-
   end
 end
